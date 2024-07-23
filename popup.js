101d63bb2f1a8afa1f372a649658fc7eb5ada940
Vlad Permaz <141409966+VladimirGagarin@users.noreply.github.com>
2024-07-23 08:02:08 +0700

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('myButton');
+    button.addEventListener('click', function() {
+      // Replace with your website URL
+      window.open('index.html', '_blank');
+    });
+  });
+  
